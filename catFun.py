868a7288ca82bffd7c87d269227fe79f0fc182cd
Colin Harfst <cph5wr@virginia.edu>
2015-11-19 15:36:36 -0500

    The cat will now start in the upper left quadrant with a random position and a random velocity between 0 and 3.

--- catFun.py
+++ catFun.py
@@ -2,6 +2,8 @@ import runWorld as rw
 import drawWorld as dw
 import pygame as pg
 
+from random import randint
+
 ################################################################
 
 # This program is an interactive simulation/game. A cat starts
@@ -101,7 +103,7 @@ def handleEvent(state, event):
 # World state will be single x coordinate at left edge of world
 
 # The cat starts at the left, moving right 
-initState = (0,1,0,1)
+initState = (randint(0,250),randint(1,3),randint(0,250),randint(1,3))
 
 # Run the simulation no faster than 60 frames per second
 frameRate = 60
